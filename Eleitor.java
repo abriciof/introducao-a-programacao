ae887258c59f7771e301eeca095f131abe3bca0b
Fabricio Guimaraes <fabricio15.guimaraes@gmail.com>
2024-01-08 09:33:32 -0400

    Eleitor

---
+++ Eleitor.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Eleitor{
+        private int idade;
+
+        Eleitor(int idade){
+                this.setIdade(idade);
+        }
+
+        public int getIdade(){
+                return this.idade;
+        }
+
+        public void setIdade(int idade){
+                this.idade = idade;
+        }
+
+        public void calculaClassificacao(){
+                String resultado = "eleitor facultativo";
+                if (this.getIdade() < 16){
+                        resultado = "nao votante";
+                }else if ( (this.getIdade() <= 65) && (this.getIdade() >= 18) ){
+                        resultado = "eleitor obrigatorio";
+                }
+                System.out.println(resultado);
+        }
+
+        public static void main(String[] args){
+                Scanner leitor = new Scanner(System.in);
+                int idade = leitor.nextInt();
+                Eleitor eleitor = new Eleitor(idade);
+                eleitor.calculaClassificacao();
+        }
+}
